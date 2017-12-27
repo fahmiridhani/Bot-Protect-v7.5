ddc36ead809cab36cf74e4678ac421a46a3a8eb7
rishani <33795067+rishani@users.noreply.github.com>
2017-12-26 22:58:10 -0800

    Update channel.py

--- LINETCR/Api/channel.py
+++ LINETCR/Api/channel.py
@@ -18,8 +18,8 @@ class Channel:
     http_query_path = "/S4"
     channel_query_path = "/CH4"
 
-    UA = "Line/7.14.0"
-    LA = "IOSIPAD\t7.14.0\tiPhone OS\t10.12.0"
+    UA = "Line/1.7.14"
+    LA = "CHROMEOS\t1.7.14\tChrome_OS\t1"    
 
     authToken = None
     mid = None
